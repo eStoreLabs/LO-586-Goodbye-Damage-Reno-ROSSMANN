6b4d75731d73815e4c9cebbaa8bb5cbe2e0b9b0e
Nikolas Jadziński <nikolas.jadzinski@gmail.com>
2025-06-02 15:24:19 +0200

    fix: E-retailer requirements

--- Portals/0/rich-content/garnier/3600542225373/scripts/script.js
+++ Portals/0/rich-content/garnier/3600542225373/scripts/script.js
@@ -1,5 +1,5 @@
 (() => {
-    const swiper = new Swiper('#es-garnier .es-swiper', {
+    const swiper = new Swiper('#es-rc #es-garnier .es-swiper', {
         loop: false,
         autoHeight: true,
 
@@ -7,13 +7,13 @@
         spaceBetween: 10,
 
         pagination: {
-            el: '#es-garnier .es-swiper1-pagination',
+            el: '#es-rc #es-garnier .es-swiper1-pagination',
             clickable: true,
         },
 
         navigation: {
-            nextEl: '#es-garnier .es-swiper1-button-next',
-            prevEl: '#es-garnier .es-swiper1-button-prev',
+            nextEl: '#es-rc #es-garnier .es-swiper1-button-next',
+            prevEl: '#es-rc #es-garnier .es-swiper1-button-prev',
         },
 
         breakpoints: {
